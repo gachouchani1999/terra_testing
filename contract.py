5e14fdb8f48ae41394b6a5b6c1f158ab03886c93
Georges Chouchani <gachouchani@ndu.edu.lb>
2021-11-06 00:00:07 +0200

    ghi

--- contract.py
+++ contract.py
@@ -1,25 +1,29 @@
 import base64
+from enum import auto
 from terra_sdk.client.lcd import LCDClient
 from terra_sdk.key.mnemonic import MnemonicKey
 from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract, MsgExecuteContract, msgs
 from terra_sdk.core.auth.data.tx import StdFee
+from terra_sdk.client.lcd.api.bank import BankAPI
 
 
 
-terra = LCDClient(chain_id='bombay-12', url = "https://lcd.terra.dev")
-contract = './cw721_base.wasm'
+terra = LCDClient(chain_id='Bombay-12', url = "https://bombay-lcd.terra.dev")
 
 
-mk = MnemonicKey()
+contract = './collectxyz_nft_contract.wasm'
+
+
+mk = MnemonicKey('used dynamic degree traffic inject various ready fluid federal toilet valid marine practice all blouse tide stomach object food wool suspect economy swim ketchup')
 print(mk)
 wallet1 = terra.wallet(mk)
-
+print(terra.bank.balance(wallet1.key.acc_address))
 contract_file = open(contract,'rb')
 file_bytes = base64.b64encode(contract_file.read()).decode()
 store_code = MsgStoreCode(wallet1.key.acc_address, file_bytes)
 store_code_tx = wallet1.create_and_sign_tx(
     msgs = [store_code],
-    gas_adjustment= 1.5,
+    fee=StdFee(6000000, "1000000uluna")
 )
 
 store_code_tx_result = terra.tx.broadcast(store_code_tx)
